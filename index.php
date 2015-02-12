502bdd297e11aa43bb74be17a0c0de452c03affe
Tepes Filip <tepes.filip@outlook.com>
2015-02-12 23:08:18 +0200

    doar header+test

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	<script src="jquery.js"></script>
+</head>
+<body>
+
+<div id="header">
+
+ <table style="width:100%">
+  <tr>
+    <td>Acasa</td>
+    <td>Probleme</td>
+    <td>Lectii</td>
+  	<td>Clasament</td>
+</table> 
+
+
+</div>
+
+
+
+</body>
+</html>
